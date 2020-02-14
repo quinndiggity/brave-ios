6dbd9d857bf2083c1f9ca1f6ef06c5b26debfdfd
Brandon <JustBrandonT@gmail.com>
2020-02-14 13:31:57 -0500

    Add a global preference to enable/disable safe-browsing.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -3412,6 +3412,7 @@ extension BrowserViewController: PreferencesObserver {
              Preferences.Shields.httpsEverywhere.key,
              Preferences.Shields.blockScripts.key,
              Preferences.Shields.blockPhishingAndMalware.key,
+             Preferences.Shields.googleSafeBrowsing.key,
              Preferences.Shields.blockImages.key,
              Preferences.Shields.fingerprintingProtection.key,
              Preferences.Shields.useRegionAdBlock.key:
